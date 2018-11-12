4497166fb5ad6b8f63b0b28fcc63648baf9fc126
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-12 13:13:10 -0200

    Create   1006-Média_2.c

---
+++ 1-Iniciante/1006-Média_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    double N1,N2,N3,MED;
+            scanf("%lf",&N1);
+            scanf("%lf",&N2);
+            scanf("%lf",&N3);
+        MED = ((2*N1)+(3*N2)+(5*N3))/10;
+        printf("MEDIA = %.1f\n",MED);
+    return 0;
+}
