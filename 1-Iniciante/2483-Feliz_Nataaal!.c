d146d93d096bf2827455198abab37649dc8ab3d3
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:37:54 -0200

    Create 2483-Feliz_Nataaal!.c

---
+++ 1-Iniciante/2483-Feliz_Nataaal!.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+	int k,cont;
+	scanf("%d",&k);
+	system("cls");
+	printf("Feliz nat");
+	for(cont=0;cont<k;cont++){
+		printf("a");
+	}
+	printf("l!\n");
+	return(0);
+}
