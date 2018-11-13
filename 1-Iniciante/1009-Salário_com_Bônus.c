59843947ba1d804a98dbdeca5df6c6b06d2a5cd1
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:00:59 -0200

    Update 1009-Salário_com_Bônus.c

--- 1-Iniciante/1009-Salário_com_Bônus.c
+++ 1-Iniciante/1009-Salário_com_Bônus.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+ 	char nome[30];
+	double fixo,vendas,comiss;
+	gets(nome);
+	fflush(stdin);
+	scanf("%lf",&fixo);
+	scanf("%lf",&vendas);
+	fixo = fixo + (15*vendas/100) ;
+	printf("TOTAL = R$ %.2lf\n",fixo);
+    return 0;
+}
