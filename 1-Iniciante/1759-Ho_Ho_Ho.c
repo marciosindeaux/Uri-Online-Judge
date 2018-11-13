2d25cef8267c6a31b2a91a03c4ec182d1684e197
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:32:34 -0200

    Create 1759-Ho_Ho_Ho.c

---
+++ 1-Iniciante/1759-Ho_Ho_Ho.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+	int N,cont;
+	scanf("%d",&N);
+	printf("Ho");
+	if(N>=2){
+		for(cont=2;cont<=N;cont++){
+			printf(" ");
+			printf("Ho");
+		}
+	}
+	printf("!\n");
+}
