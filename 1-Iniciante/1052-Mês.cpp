3eaee3810d79b7b01bc92ad2e608799a8c0b1411
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:07:21 -0200

    Create 1052-Mês.cpp

---
+++ 1-Iniciante/1052-Mês.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int mes;
+    cin>>mes;
+    if(mes == 1){
+        cout<<"January"<<endl;
+        return 0;
+    }
+    if(mes == 2){
+        cout<<"February"<<endl;
+        return 0;
+    }
+    if(mes == 3){
+        cout<<"March"<<endl;
+        return 0;
+    }
+    if(mes == 4){
+        cout<<"April"<<endl;
+        return 0;
+    }
+    if(mes == 5){
+        cout<<"May"<<endl;
+        return 0;
+    }
+    if(mes == 6){
+        cout<<"June"<<endl;
+        return 0;
+    }
+    if(mes == 7){
+        cout<<"July"<<endl;
+        return 0;
+    }
+    if(mes == 8){
+        cout<<"August"<<endl;
+        return 0;
+    }
+    if(mes == 9){
+        cout<<"September"<<endl;
+        return 0;
+    }
+    if(mes == 10){
+        cout<<"October"<<endl;
+        return 0;
+    }
+    if(mes == 11){
+        cout<<"November"<<endl;
+        return 0;
+    }
+    if(mes == 12){
+        cout<<"December"<<endl;
+        return 0;
+    }
+}
