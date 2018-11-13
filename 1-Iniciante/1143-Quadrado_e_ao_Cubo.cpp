44ce983d0a51b0e9e20409e0d1a9c804d3ec07fc
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:26:35 -0200

    Create 1143-Quadrado_e_ao_Cubo.cpp

---
+++ 1-Iniciante/1143-Quadrado_e_ao_Cubo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int c,cont;
+    cin>>c;
+    for(cont =1;cont<=c;cont++){
+        cout<<cont<<" "<<cont*cont<<" "<<cont*cont*cont<<endl;
+    }
+    return 0;
+}
