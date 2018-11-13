5200805f60d59b0c87f0456163febf5fe810feec
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:38:35 -0200

    Create 2717-Tempo_do_Duende.cpp

---
+++ 1-Iniciante/2717-Tempo_do_Duende.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n,a,b;
+   cin>>n;
+   cin>>a>>b;
+   if(a+b>n){
+       cout<<"Deixa para amanha!"<<endl;
+   }else{
+       cout<<"Farei hoje!"<<endl;
+   }
+   return(0);
+}
