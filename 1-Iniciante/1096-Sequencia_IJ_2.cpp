d160cf0a2931737d598b056980ddcb06194ab1f0
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-11-13 09:23:07 -0200

    Create 1096-Sequencia_IJ_2.cpp

---
+++ 1-Iniciante/1096-Sequencia_IJ_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int cont=1,p=7;;
+    for(cont;cont<=9;cont=cont+2){
+        cout<<"I="<<cont<<" J="<<p<<endl;
+        cout<<"I="<<cont<<" J="<<p-1<<endl;
+        cout<<"I="<<cont<<" J="<<p-2<<endl;
+    }
+    return 0 ;
+}
