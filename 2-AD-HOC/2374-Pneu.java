aae7b1705cb7f108d7872adeb7b469ecffe472ad
Marcio Alexandre Freire Sindeaux <39840562+marciosindeaux@users.noreply.github.com>
2018-12-27 09:10:22 -0200

    Create 2374-Pneu.java

---
+++ 2-AD-HOC/2374-Pneu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 2374-Pneu
+{
+    static Scanner e = new Scanner(System.in);
+    public static void main(String[] args) {
+        int v1 = e.nextInt();
+        int v2 = e.nextInt();
+        System.out.println(v1-v2);
+    }
+}
